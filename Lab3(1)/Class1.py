92f9cc6e8ec72362f01876056ea7ccc1994afe7f
salamatmuldashov <98634520+salamatmuldashov@users.noreply.github.com>
2022-02-19 11:06:52 +0600

    Add files via upload

---
+++ Lab3(1)/Class1.py
@@ -0,0 +1,11 @@
+class Myclass():
+    def getString(self):
+        self.s = input()
+    def printString(self):
+        print(self.s.upper())
+
+s = Myclass()
+s.getString()
+s.printString()
+
+        
